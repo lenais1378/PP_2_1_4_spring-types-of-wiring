dc54eb350d4f90f11294ac47a0a931971ecbc1c5
lenais1378 <lenais1378@gmail.com>
2022-10-12 15:27:15 +0300

    little changes beans

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -4,11 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-@Component
+
 
 public class Duck5 {
     private Egg6 egg;
-    @Autowired
+
     public Duck5(Egg6 egg) {
         this.egg = egg;
     }
